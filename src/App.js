90ab5cac7188609480d670c6b48a2295c483b9ab
Fereshta Alavy <fereshta.alavy@sjsu.edu>
2020-07-14 14:53:41 -0700

    retrieve images from firbase storage and display it on web

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React, { useState } from "react";
 import axios from "axios";
 import "./App.css";
+import { storage } from "./firebase";
 
 function App() {
   const [selectedFile, setSelectedFile] = useState(null);
+  const [imgUrl, setImgUrl] = useState(null);
+  const [images, setImages] = useState({});
 
   function fileSelecterHandler(event) {
     setSelectedFile(event.target.files[0]);
   }
 
   function fileUploadHandler() {
+    // setImgUrl(URL.createObjectURL(selectedFile));
     const fd = new FormData();
     fd.append("image", selectedFile, selectedFile.name);
     axios
@@ -18,15 +22,38 @@ function App() {
         fd
       )
       .then(res => {
-        console.log(res);
+        storage.listAll().then(res => {
+          console.log(typeof res);
+          res.items.forEach(item =>
+            storage
+              .child(item.location.path)
+              .getDownloadURL()
+              .then(url => {
+                const oldImages = images;
+                oldImages[url] = null;
+                setImages({ ...oldImages });
+              })
+          );
+        });
       });
   }
 
+  console.log(images);
+
   return (
     <div className="App">
-      <input type="file" onChange={fileSelecterHandler} />
-
-      <button onClick={fileUploadHandler}>Upload</button>
+      <input
+        className="choose-file"
+        type="file"
+        onChange={fileSelecterHandler}
+      />
+      <button className="upload" onClick={fileUploadHandler}>
+        Upload
+      </button>
+      <br />
+      {Object.keys(images).map(url => {
+        return <img src={url} height="300" width="300" />;
+      })}
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import * as firebase from "firebase";
+import "firebase/storage";
+var firebaseConfig = {
+  apiKey: "AIzaSyAiE3VmRK--LfGxtNz8u8PuMFMD6tBH8sw",
+  authDomain: "donate4free.firebaseapp.com",
+  databaseURL: "https://donate4free.firebaseio.com",
+  projectId: "donate4free",
+  storageBucket: "donate4free.appspot.com",
+  messagingSenderId: "36445766881",
+  appId: "1:36445766881:web:62675b5509cdbcc48763a0",
+  measurementId: "G-XVTJYNC4V1"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const storage = firebase.storage().ref();
+
+export { storage, firebase as default };
